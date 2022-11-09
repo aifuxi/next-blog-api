1100a3cdf34cc601d501af54812b603937b28f16
aifuxi <aifuxi.js@gmail.com>
2022-11-10 04:08:47 +0800

    fix: 上传文件后返回的路径加上https前缀

--- apps/admin/src/v1/file/file.service.ts
+++ apps/admin/src/v1/file/file.service.ts
@@ -20,7 +20,7 @@ export class FileService {
         Key: nanoid(4) + file.originalname /* 必须, 可以看成是上传后的文件名 */,
         Body: file.buffer, // 上传文件对象
       });
-      url = res.Location;
+      url = `https://${res.Location}`;
     } else {
       url = `${ADMIN_API}/${file.filename}`;
     }
