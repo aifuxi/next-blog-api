5c84b701074b90284bc2da77c798299ce58ccf09
aifuxi <aifuxi.js@gmail.com>
2022-11-10 03:28:41 +0800

    fix: prod环境不使用本地上传

--- apps/admin/src/v1/file/file.module.ts
+++ apps/admin/src/v1/file/file.module.ts
@@ -3,26 +3,26 @@ import { FileService } from './file.service';
 import { FileController } from './file.controller';
 import { MulterModule } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { format } from 'date-fns';
 import { nanoid } from 'nanoid';
-import { join } from 'path';
 import { UPLOAD_FOLDER } from '@libs/common/constants/upload';
 
 @Module({
-  imports: [
-    process.env.NODE_ENV !== 'production' &&
-      MulterModule.register({
-        storage: diskStorage({
-          // 配置文件上传后的文件夹路径
-          destination: UPLOAD_FOLDER,
-          filename: (req, file, cb) => {
-            // 在此处自定义保存后的文件名称
-            const filename = `${nanoid()}.${file.mimetype.split('/')[1]}`;
-            return cb(null, filename);
-          },
-        }),
-      }),
-  ],
+  imports:
+    process.env.NODE_ENV !== 'production'
+      ? [
+          MulterModule.register({
+            storage: diskStorage({
+              // 配置文件上传后的文件夹路径
+              destination: UPLOAD_FOLDER,
+              filename: (req, file, cb) => {
+                // 在此处自定义保存后的文件名称
+                const filename = `${nanoid()}.${file.mimetype.split('/')[1]}`;
+                return cb(null, filename);
+              },
+            }),
+          }),
+        ]
+      : [],
   controllers: [FileController],
   providers: [FileService],
 })
